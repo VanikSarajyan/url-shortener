2f50d5e4f97350eca35d81aa91cbab22f0d37afa
VanikSarajyan <vaniksarajyan@gmail.com>
2023-01-05 19:31:49 +0400

    changed app.get and post

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import random
 import string
 import json
-from flask import Flask, request, redirect
+from flask import Flask, request, redirect, make_response
 
 app = Flask(__name__)
 
@@ -10,24 +10,26 @@ url_dict = {}
 def generate_random_string(length: int = 6) -> str:
     return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))  
 
-def save_url(long_url: str) -> None:
+def save_url(long_url: str) -> str:
     short_url = generate_random_string()
     while short_url in url_dict:
         short_url = generate_random_string()
     url_dict[short_url] = long_url
 
+    return short_url
 
-@app.post('/shorten')
+
+@app.route('/shorten', methods=['POST'])
 def shorten():
     data = json.loads(request.data)
-    save_url(data['long_url'])
+    short_url = save_url(data['long_url'])
 
-    return url_dict
+    return short_url
 
-@app.get('/<short_url>')
+@app.route('/<short_url>')
 def goto(short_url):
     if short_url in url_dict:
-        return redirect(f'http://{url_dict[short_url]}')
+        return redirect(f'{url_dict[short_url]}')
     else:
         return f"Invalid short url. {short_url}"
 
